8ccabfc52ed5a22a45464cd77f9685705aef4968
ezrateshale <ezra.teshale12@gmail.com>
2026-06-11 15:27:51 -0500

    Initial commit

---
+++ src/main/java/nl/pluralsight/stagepass/controller/BookingController.java
@@ -0,0 +1,52 @@
+package nl.pluralsight.stagepass.controller;
+
+import jakarta.validation.Valid;
+import nl.pluralsight.stagepass.model.Booking;
+import nl.pluralsight.stagepass.service.BookingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        return ResponseEntity.ok(bookingService.getAllBookings());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> getBookingById(@PathVariable Long id) {
+        return bookingService.getBookingById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/concert/{concertId}")
+    public ResponseEntity<List<Booking>> getBookingsByConcert(@PathVariable Long concertId) {
+        return ResponseEntity.ok(bookingService.getBookingsByConcert(concertId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+        Booking created = bookingService.createBooking(booking);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);//201
+        //return ResponseEntity.ok(created); 200
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelBooking(@PathVariable Long id) {
+        bookingService.cancelBooking(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
+}
